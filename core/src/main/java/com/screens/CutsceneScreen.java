2340da8c4ccb750e9e3700a0ac0ff9fbf38481e0
Vessalia <nathanielples@outlook.com>
2023-05-01 11:52:31 -0500

    ability to skip cutscenes

--- core/src/main/java/com/screens/CutsceneScreen.java
+++ core/src/main/java/com/screens/CutsceneScreen.java
@@ -2,6 +2,7 @@ package com.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -100,6 +101,15 @@ public class CutsceneScreen extends GameScreen
         {
             currImageIndex++;
             imageTimer = IMAGE_DURATION;
+        }
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE) && type == CutsceneType.INTRO)
+        {
+            game.setScreen(new GameplayScreen(game));
+        }
+        else if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
+        {
+            game.setScreen(new MainMenuScreen(game));
         }
     }
 
